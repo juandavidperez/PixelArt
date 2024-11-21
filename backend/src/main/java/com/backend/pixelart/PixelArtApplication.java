44e8f06158299b3c35722aa5272c38d9b927c1ef
Yeider <yeidergarzon3@gmail.com>
2024-11-20 19:40:08 -0500

    Corrección nombre aplicacion

--- backend/src/main/java/com/backend/pixelart/PixelArtApplication.java
+++ backend/src/main/java/com/backend/pixelart/PixelArtApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PixelartApplication {
+public class PixelArtApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PixelartApplication.class, args);
+		SpringApplication.run(PixelArtApplication.class, args);
 	}
 
 }
